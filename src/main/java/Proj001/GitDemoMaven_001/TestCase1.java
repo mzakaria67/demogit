30ceb2b4bd6f7998fc1b3c3b87e2da9d6f8b2959
Arpana <muriki.arpana@gmail.com>
2022-05-18 06:57:50 +0530

    Second Commit

---
+++ src/main/java/Proj001/GitDemoMaven_001/TestCase1.java
@@ -0,0 +1,10 @@
+package Proj001.GitDemoMaven_001;
+
+public class TestCase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Git and Github...");
+	}
+
+}
