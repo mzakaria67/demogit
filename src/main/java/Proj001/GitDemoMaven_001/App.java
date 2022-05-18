3352bda6c66e0f4500e9b9653534b35a893514f8
Arpana <muriki.arpana@gmail.com>
2022-05-18 06:54:56 +0530

    First Commit

---
+++ src/main/java/Proj001/GitDemoMaven_001/App.java
@@ -0,0 +1,13 @@
+package Proj001.GitDemoMaven_001;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
