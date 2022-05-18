a0c26ffdca9f391e82eed6519232ae868376013f
mzakaria67 <94887374+mzakaria67@users.noreply.github.com>
2022-05-17 22:04:10 -0400

    first comment

---
+++ src/main/java/Proj001/GitDemoMaven_001/stamp.java
@@ -0,0 +1,12 @@
+package Proj001.GitDemoMaven_001;
+
+public class stamp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(" welcome to git world ");
+
+	}
+
+}
